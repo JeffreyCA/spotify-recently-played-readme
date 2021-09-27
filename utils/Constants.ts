f1b751476223cb166b565b565270827282422811
Seto <setoelkahfi@gmail.com>
2021-09-27 08:49:03 +0200

    Prefix env variables with NEXT_PUBLIC_

--- utils/Constants.ts
+++ utils/Constants.ts
@@ -1,9 +1,9 @@
 // Spotify app client ID
-export const ClientId = process.env.CLIENT_ID;
+export const ClientId = process.env.NEXT_PUBLIC_CLIENT_ID;
 // Vercel app base URL
-export const BaseUrl = process.env.BASE_URL;
+export const BaseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 // Redirect URI
-export const RedirectUri = process.env.REDIRECT_URI;
+export const RedirectUri = process.env.NEXT_PUBLIC_REDIRECT_URI;
 
 export const defaultCount = 5;
 export const defaultUniqueTrack = false;
